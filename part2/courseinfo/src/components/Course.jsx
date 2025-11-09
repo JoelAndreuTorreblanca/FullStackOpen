cf637914ebb7d772ea1145dc6b37e54cddd21694
4141gtppuKovuKov# <joelandreu00@gmail.com>
2025-11-09 21:03:25 +0100

    Created 'part2' and 'courseinfo' and Exercise 2.1

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+import Content from "./Content";
+
+export default function Course({ course }) {
+  return (
+    <>
+      <Header title={ course.name } />
+      <Content parts={ course.parts } />
+    </>
+  )
+}
