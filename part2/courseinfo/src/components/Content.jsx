195309723d6e01eb5b1252fef45183b2f77ed2f2
4141gtppuKovuKov# <joelandreu00@gmail.com>
2025-11-09 21:09:18 +0100

    Exercise 2.2

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -1,11 +1,15 @@
 import Part from "./Part";
 
 export default function Content({ parts }) {
+  const allExercises = parts.map(part => part.exercises);
+  const totalExercises = allExercises.reduce((partialSum, a) => partialSum + a, 0);
+
   return (
     <div>
         {parts.map(part => (
             <Part key={ part.id } part={ part } />
         ))}
+        <p><strong>total of {totalExercises} exercises</strong></p>
     </div>
   )
 }
